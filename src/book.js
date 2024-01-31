b0ab83236398c2d4d08ac40014e49b6d55422095
Zach Wolek <zach.wolek@gmail.com>
2024-01-31 17:44:40 -0600

    show the completion of 5/18

--- src/book.js
+++ src/book.js
@@ -4,10 +4,20 @@ function createTitle(title) {
 
 var bookIdea = console.log(createTitle("Storm's Awakening"))
 
+function buildMainCharacter(nameChar, ageChar, pronounsChar){
+  return {
+    name: nameChar,
+    age: ageChar, 
+    pronouns: pronounsChar
+  }
+}
+var ghoulCharacter = console.log(buildMainCharacter("Vassya", 16, "she/her")); 
+var bakerCharacter = console.log(buildMainCharacter("Hannah", 30, "she/her")); 
+
 
   module.exports = {
   createTitle, 
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,

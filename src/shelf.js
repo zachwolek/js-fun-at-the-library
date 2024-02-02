fa76706f8212f506b5d47314ba5facce159d60d4
Zach Wolek <zach.wolek@gmail.com>
2024-02-01 21:28:51 -0600

    Show 8/11 completion on shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,28 @@
+function shelfBook(book, sciFiShelf){
+  if (sciFiShelf.length < 3){
+  sciFiShelf.unshift(book);
+  }
+  return sciFiShelf
+}
+
+function unshelfBook(bookTitle, sciFiShelf){
+  for (i = 0; i < sciFiShelf.length; i++){
+    if (sciFiShelf[i].title === bookTitle){
+      sciFiShelf.splice(i, 1)
+    }
+  }
+  return sciFiShelf
+}
+
+function listTitles(){}
+
+// var sciFiShelf = [];
+//   sciFiShelf.push(book);
+//   return sciFiShelf
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+ unshelfBook,
+  listTitles,
   // searchShelf
 };
